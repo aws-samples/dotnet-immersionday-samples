9aa457adbbda74d9ebaf70a548ad224bb20e23e6
Craig Bossie <cbbossie@amazon.com>
2024-03-01 10:01:21 -0500

    Update AWS Toolkit

--- src/OnAfterInit.ps1
+++ src/OnAfterInit.ps1
@@ -11,7 +11,7 @@ cd $dir
 $Installer = "chrome_installer.exe";
 Invoke-WebRequest "http://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile $Installer;
 Start-Process -FilePath $dir$Installer -Args "/silent /install" -Verb RunAs -Wait;
-Remove-Item $dir$Installer;
+# Remove-Item $dir$Installer;
 
 
 #Update Visual Studio Installer
@@ -19,8 +19,26 @@ Remove-Item $dir$Installer;
 $Installer = "vs_community.exe";
 Invoke-WebRequest "https://aka.ms/vs/17/release/vs_community.exe" -OutFile $Installer;
 Start-Process -FilePath $dir$Installer -Args "--quiet --update" -Verb RunAs -Wait;
-Remove-Item $dir$Installer
+# Remove-Item $dir$Installer
 
 #Update Visual Studio
-cd 'C:\Program Files (x86)\Microsoft Visual Studio\Installer'
-.\setup.exe update --force --quiet --productid 'Microsoft.VisualStudio.Product.Community' --channelId 'VisualStudio.17.Release'
+Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" -Verb RunAs -Args " update --force --quiet --productid 'Microsoft.VisualStudio.Product.Community' --channelId 'VisualStudio.17.Release'" -Wait
+
+#Update AWS Toolkit for Visual Studio
+$Installer = "AWSToolkitPackage.v17.vsix"
+Invoke-WebRequest "https://amazonwebservices.gallerycdn.vsassets.io/extensions/amazonwebservices/awstoolkitforvisualstudio2022/1.48.0.0/1708627515853/AWSToolkitPackage.v17.vsix" -OutFile $Installer
+Start-Process -FilePath "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\VSIXInstaller.exe" -Verb RunAs -Args "/quiet $dir$Installer" -Wait
+
+
+
+
+
+
+
+
+
+
+
+
+
+
