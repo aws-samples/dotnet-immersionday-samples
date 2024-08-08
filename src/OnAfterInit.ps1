2f03d07a2335d54ced217a97f7dffe2c13f91a9b
Craig Bossie <cbbossie@amazon.com>
2024-08-08 12:17:10 -0400

    Update Microservice Extractor for .NET

--- src/OnAfterInit.ps1
+++ src/OnAfterInit.ps1
@@ -28,9 +28,17 @@ $Installer = "AWSToolkitPackage.v17.vsix"
 Invoke-WebRequest "https://amazonwebservices.gallerycdn.vsassets.io/extensions/amazonwebservices/awstoolkitforvisualstudio2022/1.56.0.0/1722549125222/AWSToolkitPackage.v17.vsix" -OutFile $Installer
 Start-Process -FilePath "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\VSIXInstaller.exe" -Verb RunAs -Args "/quiet $dir$Installer" -Wait
 
+#Update Microservice Extractor
+$Installer = "ServiceExtract.exe"
+Invoke-WebRequest "https://s3.us-west-2.amazonaws.com/aws.serviceextract.download/latest/windows/ServiceExtract.exe" -OutFile $Installer
+Start-Process -FilePath $dir$Installer -Args "/S" -Verb RunAs -Wait;
 
+$ShortCutTarget = "C:\Users\Administrator\AppData\Local\Programs\AWS Microservice Extractor for .NET\AWS Microservice Extractor for .NET.exe"
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\AWS Microservice Extractor for .NET.lnk")
+$Shortcut.TargetPath = $ShortCutTarget
+$Shortcut.Save()
 
-
-
-
-
+$StartShortcut = $WshShell.CreateShortcut("$Home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\AWS Microservice Extractor for .NET.lnk")
+$StartShortcut.TargetPath = "C:\Users\Administrator\AppData\Local\Programs\AWS Microservice Extractor for .NET\AWS Microservice Extractor for .NET.exe"
+$StartShortcut.Save()
